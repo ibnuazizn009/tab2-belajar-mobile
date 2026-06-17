beb2cba37e53bd120bba74e93f40839f59c9f5f0
Ibnu Aziz N <99677229+ibnuazizn009@users.noreply.github.com>
2026-06-18 00:50:19 +0700

    .

--- backend/resources/views/landing.blade.php
+++ backend/resources/views/landing.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Tabungan Sekolah - Solusi Digital Pencatatan Tabungan Siswa</title>
 
-    <link rel="icon" type="image/x-icon" href="/favicon_b.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon_b.png">
+
 
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
