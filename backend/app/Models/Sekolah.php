9022a3bbbf1bd164e47ca925b937e3f0b33751bc
Ibnu Aziz N <99677229+ibnuazizn009@users.noreply.github.com>
2026-06-25 21:44:14 +0700

    .

--- backend/app/Models/Sekolah.php
+++ backend/app/Models/Sekolah.php
@@ -24,6 +24,7 @@ class Sekolah extends Model
         'is_premium',
         'paket_layanan',         
         'premium_expires_at',
+        'status_pembayaran',
     ];
 
     protected $casts = [
